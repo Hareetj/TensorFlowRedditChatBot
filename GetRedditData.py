e785a8cde237f36f927dab6d3fe2b0bf0beba149
Hareet Jhutty (hjhutty) <hjhutty@cisco.com>
2017-06-12 14:32:48 -0700

    working on dataset scripts

--- GetRedditData.py
+++ GetRedditData.py
@@ -1,10 +1,24 @@
 import praw
+
 import secrets
 
-reddit = praw.Reddit(client_id= secrets.client_id,
-                     client_secret= secrets.client_secret,
-                     password= secrets.password,
-                     user_agent='chatbot',
-                     username= secrets.user)
 
-print (reddit.user.me())
+class GenData(object):
+    def __init__(self, subreddit):
+        self.subreddit = subreddit
+        self.reddit = praw.Reddit(client_id= secrets.client_id,
+                             client_secret= secrets.client_secret,
+                             password= secrets.password,
+                             user_agent='chatbot',
+                             username= secrets.user)
+    def getComments(self):
+        subreddit = self.reddit.subreddit(self.subreddit)
+        for comment in subreddit.stream.comments():
+            if ('*' not in comment.body):
+                print (comment.body)
+def main():
+    askreddit = GenData("askreddit")
+    askreddit.getComments()
+
+if __name__ == '__main__':
+	main()
